c4f04bc1af98b385b0f79f07347ff00fda9c0aad
fraizis <fraizis@mail.ru>
2025-07-18 20:14:03 +0300

    Sync_service

--- get_yandex_info.py
+++ get_yandex_info.py
@@ -8,9 +8,9 @@ from config import TOKEN
 URL = "https://cloud-api.yandex.net/v1/disk/resources"
 
 HEADERS = {'Content-Type': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
-SAVE_DIR = 'test'
+CLOUD_DIR = 'test'
 
-directory = os.path.join(os.path.abspath(os.getcwd()), SAVE_DIR)
+directory = os.path.join(os.path.abspath(os.getcwd()), 'test')
 
 print(directory)
 
@@ -32,42 +32,69 @@ def get_cloud_files(path):
     dir_files = result.json()['_embedded']['items']
     cloud_files = {}
     for i in dir_files:
-        #print(type(i['created']))
-        new_time = datetime.strptime(i['created'][:-6], "%Y-%m-%dT%H:%M:%S") + timedelta(hours = 3)
+        # print(type(i['created']))
+        new_time = datetime.strptime(i['created'][:-6], "%Y-%m-%dT%H:%M:%S") + timedelta(hours=3)
         cloud_files[i['name']] = datetime.strftime(new_time, "%Y-%m-%d %H:%M:%S")
-        #print('new_time',new_time)
+        # print('new_time',new_time)
 
-        #print(i['name'], i['created'])
+        # print(i['name'], i['created'])
 
     return cloud_files
 
 
-
-def create_folder(path):
-    requests.put(f'{URL}?path={path}', headers=HEADERS)
-
-
-def delete_folder(path):
-    requests.delete(f'{URL}?path={path}', headers=HEADERS)
+def create_folder(folder):
+    requests.put(f'{URL}?path={folder}', headers=HEADERS)
 
 
-def upload_file(loadfile, replace=True):
-    """Загрузка файла.
-    loadfile: Путь к загружаемому файлу
-    replace: true or false Замена файла на Диске"""
-    res = requests.get(f'{URL}/upload?path={SAVE_DIR}/{loadfile}&overwrite={replace}', headers=HEADERS).json()
-    with open(loadfile, 'rb') as f:
+def delete_file(file, cloud_dir):
+    requests.delete(f'{URL}?path={cloud_dir}/{file}', headers=HEADERS)
+
+
+def upload_file(file, cloud_dir, replace=True):
+    res = requests.get(f'{URL}/upload?path={cloud_dir}%2f{file}&overwrite={replace}', headers=HEADERS).json()
+    with open(file, 'rb') as f:
         try:
-            requests.put(res['href'], files={'file':f})
+            requests.put(res['href'], files={'file': f})
         except KeyError:
             print(res)
 
+
+def select_files_to_upload(self_files, cloud_files):
+    files_to_upload = []
+
+    for file in self_files:
+        if file in cloud_files:
+            if cloud_files[file] < self_files[file]:
+                files_to_upload.append(file)
+                del cloud_files[file]
+        else:
+            files_to_upload.append(file)
+
+    print('files_to_upload:', files_to_upload)
+    return files_to_upload
+
+
 self_files = get_self_folder_files(directory)
-cloud_files = get_cloud_files(SAVE_DIR)
+cloud_files = get_cloud_files(CLOUD_DIR)
 
 print(self_files)
 print(cloud_files)
-#create_folder('test/12345.docx')
-#delete_folder('test/12345.docx')
 
-#upload_file('logic.py')
+# [delete_file(i) for i in files_to_delete_from_cloud]
+
+# if len(cloud_files) > len(self_files):
+files_to_delete_from_cloud = cloud_files.keys() - self_files.keys()
+print(files_to_delete_from_cloud)
+
+select_files_to_upload(self_files, cloud_files)
+
+#create_folder('test/123')
+#delete_file('123456', CLOUD_DIR)
+
+#upload_file('logic.py', CLOUD_DIR)
+
+self_files = get_self_folder_files(directory)
+cloud_files = get_cloud_files(CLOUD_DIR)
+
+print(self_files)
+print(cloud_files)
